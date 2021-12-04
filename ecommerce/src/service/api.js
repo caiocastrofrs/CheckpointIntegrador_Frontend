5a7a5673dacdf2d48e49a388e4ae2a409d7cf863
caiocastrofrs <caiocastrofrs@gmail.com>
2021-12-04 10:20:59 -0300

    mudando link da api

--- ecommerce/src/service/api.js
+++ ecommerce/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-163-100-219.compute-1.amazonaws.com:8080'
+  baseURL: 'http://ec2-52-91-104-174.compute-1.amazonaws.com:8080'
 });
 
 export default api;
