a8583a3a6dea50023349d01ab830f1e6a1966a03
Larissa <nuneslari21@gmail.com>
2021-12-05 11:14:43 -0300

    Equipe

--- ecommerce/src/pages/Sobre/index.js
+++ ecommerce/src/pages/Sobre/index.js
@@ -2,11 +2,15 @@ import "./style.scss";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Equipe from '../../components/Equipe';
+import { Helmet } from 'react-helmet';
 
 const Sobre = () => {
 
   return (
     <>
+      <Helmet>
+        <title>Bodega | Sobre nós</title>
+      </Helmet>
       <Header />
       <Equipe />
       <Footer />
