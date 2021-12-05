75c85806f39c023de71bd508957ecb1f0edfde6d
GasparPSousa <gasparufrj@gmail.com>
2021-12-05 15:58:48 -0300

    Preparando o index do Produto para receber os dados da API e modificando o estilo dos botoes Comprar Agora e Adicionar ao Carrinho

--- ecommerce/src/pages/Produto/index.js
+++ ecommerce/src/pages/Produto/index.js
@@ -45,7 +45,32 @@ const produtos = [
 ]
 
 
+
+
+
 const Produto = () => {
+
+    
+    const produto = [
+
+        {
+            "id": 1,
+            "titulo": "Máquina de lavar automática Electrolux",
+            "preco": 1524.0,
+            "descricao": "Essencial Care LES11 branca 11kg 127V",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_798413-MLA43117842453_082020-O.webp",
+            "categoria": {
+                "id": 1,
+                "nomeCategoria": "ELETRODOMESTICOS"
+            }
+        }
+    ]
+
+    const parcela = 12;
+    function calcParcela() {
+        return (produto.preco / parcela).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    }
+
 
 
     return (
@@ -55,24 +80,30 @@ const Produto = () => {
             </Helmet>
             <Header />
             <h2 className="title"> Console Xbox Series S 512gb </h2>
+            {/* <h2 className="title"> {produto.titulo} </h2> */}
             <div className="container_produto">
                 <div className="container_imagem">
                     <img className="imagem" src="https://http2.mlstatic.com/D_NQ_NP_939935-MLB48431344222_122021-O.webp" alt="" />
+                    {/* <img className="imagem" src={produto.imagem} alt="" /> */}
                 </div>
                 <div className="container_descricao">
                     <div className="container_precoDesconto">
                         <p className="preco">R$ 2.499</p>
+                        {/* <p className="preco">{produto.preco}</p> */}
                         <p className="desconto">20% off</p>
                     </div>
                     <p className="dozeVezes">12xR$124,92 sem juros</p>
+                    {/* <p className="dozeVezes">{parcela}x{calcParcela()} sem juros</p> */}
                     <p className="freteGratis">Frete Grátis</p>
-                    <p className="descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas aspernatur cum tempore nihil necessitatibus explicabo, unde animi, quaerat aliquam voluptatum consectetur voluptate eaque corrupti iusto possimus eius ratione blanditiis. Mollitia?</p>
-                    {/* <div className="container_botao">
-                        <a className="buttonComprar" href="#">Comprar agora</a>
-                   </div> */}
+                    <p className="descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas aspernatur cum tempore nihil necessitatibus explicabo, unde animi, </p>
+                    {/* <p className="descricao">{produto.descricao}</p> */}
 
                     <div className="container_botao">
-                        <a className="buttonAdicionar" href="#">Adicionar ao carrinho</a>
+                        <a className="buttonComprar" href="#">Comprar agora</a>
+                   </div>
+
+                    <div className="container_botao">
+                        <a type="submit" className="buttonAdicionar" href="#">Adicionar ao carrinho</a>
                     </div>
 
                 </div>
@@ -80,9 +111,9 @@ const Produto = () => {
 
             <p className="quemViu">Quem viu esse produto também comprou...</p>
             <Container fluid={true}>
-            
+
                 <Row className="linha-produtos">
-                    
+
                     {
                         produtos.map(item => {
                             return (
