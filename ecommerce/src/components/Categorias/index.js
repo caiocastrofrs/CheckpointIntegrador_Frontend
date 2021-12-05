f0245bd2f1854929820db46a585433f469402310
caiocastrofrs <caiocastrofrs@gmail.com>
2021-12-05 12:15:03 -0300

    colocando um map nos ícones na seção de categoria

--- ecommerce/src/components/Categorias/index.js
+++ ecommerce/src/components/Categorias/index.js
@@ -1,6 +1,13 @@
 import "./style.scss";
-import { AiFillCamera } from "react-icons/ai";
+import { CgSmartHomeRefrigerator } from "react-icons/cg";
+import { BsSmartwatch } from "react-icons/bs";
+import { FaBabyCarriage } from "react-icons/fa";
+import { MdSportsSoccer } from "react-icons/md";
+import { CgGames } from "react-icons/cg";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
 const Categorias = () => {
+  
+  
   const categorias = [
     "ELETRODOMESTICOS",
     "ELETRONICOS",
@@ -10,15 +17,25 @@ const Categorias = () => {
     "INFORMATICA",
   ];
 
+  const icons = [
+    CgSmartHomeRefrigerator,
+    BsSmartwatch,
+    FaBabyCarriage,
+    MdSportsSoccer,
+    CgGames,
+    HiOutlineDesktopComputer,
+  ]
+  
   return (
     <>
       <section className="section-categorias">
         <h2 className="section-categorias__titulo-section ">Categorias</h2>
         <div className="section-categorias__container w-100">
-          {categorias.map((categoria) => {
+          {categorias.map((categoria, index) => {
+            let Icon = icons[index];
             return (
               <div className="section-categorias__container__categoria col-5 col-sm-3 col-md-5 col-lg-4 col-xl-4">
-                <AiFillCamera className="section-categorias__container__categoria__icon" />
+                <Icon className="section-categorias__container__categoria__icon" />
                 <span className="section-categorias__container__categoria__titulo">
                   {categoria}
                 </span>
