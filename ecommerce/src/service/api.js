8963a53f959de1bd3a6f1092a5342ed790d6c46b
caiocastrofrs <caiocastrofrs@gmail.com>
2021-12-08 16:49:33 -0300

    mudando link da api

--- ecommerce/src/service/api.js
+++ ecommerce/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-209-206-54.compute-1.amazonaws.com:8080'
+  baseURL: 'https://bodegadh.herokuapp.com'
 });
 
 export default api;
